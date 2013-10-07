037e3cbdabf7f2add5113c186643315ae3d7c878
haneuma0628 <haneuma0628@gmail.com>
2013-10-07 22:16:02 +0900

    delete count down

--- main.php
+++ main.php
@@ -8,13 +8,7 @@ require_once('/var/www/html/kbot_lab/config.php');
 $oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 /*---------- define ----------*/
-$messageArray = yaml_parse_file("message.yml");
-
-$today = (int) date("d");
-// 卒研発表会までの残り日数(FNCT.H24)
-//$limit = (date("m")==01)? 27-$today: NULL;
-// 卒研中間発表までの残り日数(FNCT.H25)
-$limit = (date("m")==10)? 31-$today: NULL;
+$messageArray = yaml_parse_file("/var/www/html/kbot_lab/message.yml");
 $randNo = rand(0,count($messageArray["normal"])-1);
 $randNi = rand(0,count($messageArray["night"])-1);
 /*---------- /define ----------*/
@@ -24,16 +18,9 @@ if (date("H")==8) {
   $message = $messageArray["mornign"][0];
 } else if (date("H")==0){
   $message = $messageArray["night"][$randNi];
-} else if (date("H")==10 && $limit!=NULL) {
-  $limit = strtoupper((string) gmp_strval($limit, 16));
-  if($limit != "1"){
-    $message = "@hnmx4 先輩、卒研発表会まで残り0x". $limit. "日ですね。";
-  } else {
-    $message = "@hnmx4 ". $messageArry["cheer"][0];
-  }
 } else {
   $message = $messageArray["normal"][$randNo];
-
+}
 
 // messageをテンプレートにつっこんでtwitterでpost
 $tweet = array( "status" => $message );
