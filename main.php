9d2a6ca7321b4d0f04f06a8e5cf6b36fce948e8a
hassakutea.com <haneuma0628@gmail.com>
2016-01-01 03:21:14 +0900

    朝のメッセージもランダムで選択、パスを変更

--- main.php
+++ main.php
@@ -3,19 +3,24 @@ date_default_timezone_set('Asia/Tokyo'); // time_zone
 
 // include
 require_once('/var/www/html/twitteroauth/twitteroauth.php');
-require_once('/var/www/html/kbot_lab/config.php');
+require_once('/var/www/html/hassakutea/kbot_lab/config.php');
 
-$oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+$oauth = new TwitterOAuth(
+	CONSUMER_KEY, CONSUMER_SECRET,
+	ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 /*---------- define ----------*/
-$messageArray = yaml_parse_file("/var/www/html/kbot_lab/message_main.yml");
-$randNo = rand(0,count($messageArray["normal"])-1);
-$randNi = rand(0,count($messageArray["night"])-1);
+$messageArray = yaml_parse_file("/var/www/html/hassakutea/kbot_lab/message_main.yml");
+$randMo = rand(0, count($messageArray["morning"])-1);
+$randNo = rand(0, count($messageArray["normal"])-1);
+$randNi = rand(0, count($messageArray["night"])-1);
 /*---------- /define ----------*/
+
+echo date("--- Y.m.d h:i:s ---")."\n";
 
 /*---------- メッセージを配列に格納 ----------*/
 if (date("H")==8) {
-  $message = $messageArray["mornign"][0];
+  $message = $messageArray["morning"][$randMo];
 } else if (date("H")==0){
   $message = $messageArray["night"][$randNi];
 } else {
@@ -25,9 +30,7 @@ if (date("H")==8) {
 // messageをテンプレートにつっこんでtwitterでpost
 $tweet = array( "status" => $message );
 $result = $oauth->post('statuses/update', $tweet);
-// debug
-//var_dump($result);
-var_dump($messageArray);
-var_dump($message);
+
+var_dump($result);
 
 ?>
--- reply.php
+++ reply.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Tokyo"); // time_zone
 
 // include
 require_once("/var/www/html/twitteroauth/twitteroauth.php");
-require_once("/var/www/html/kbot_lab/config.php");
+require_once("/var/www/html/hassakutea/kbot_lab/config.php");
 
 $oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
@@ -11,10 +11,10 @@ $oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TO
 $mentions = $oauth->get("statuses/mentions_timeline");
 
 // メッセージ取得
-$messages = yaml_parse_file("/var/www/html/kbot_lab/message_reply.yml");
+$messages = yaml_parse_file("/var/www/html/hassakutea/kbot_lab/message_reply.yml");
 
 // last_tweet_idから最後に返信したツイートのIDを取得
-$handle = fopen("/var/www/html/kbot_lab/last_tweet_id", "r");
+$handle = fopen("/var/www/html/hassakutea/kbot_lab/last_tweet_id", "r");
 if ($handle) {
   $lastTweetId = (int)fgets($handle);
   fclose($handle);
@@ -30,10 +30,10 @@ foreach ($mentions as $mention) {
   //if ($mention->user->screen_name == "hnmx4" && $mention->id > $lastTweetId) {
   if ($mention->id > $lastTweetId) {
     echo $mention->user->screen_name.":".$mention->text.":".$mention->id."\n";
-    
+
     $reply_to = $mention->user->screen_name;
     $reply_name = $mention->user->name;
-    
+
     $keyword = "励まし";
     $pos = strpos($mention->text, $keyword);
 
@@ -59,7 +59,7 @@ foreach ($mentions as $mention) {
 }
 
 if ($lastTweetId < $mentionId) {
-  $handle = fopen("/var/www/html/kbot_lab/last_tweet_id", "w");
+  $handle = fopen("/var/www/html/hassakutea/kbot_lab/last_tweet_id", "w");
   if ($handle) {
     echo "write:".$mentionId."\n";
     fwrite($handle, $mentionId);
