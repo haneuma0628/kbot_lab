3cf37c933dc2018d71bc8e2e164bd8c791ecb57e
hnmx4 <haneuma0628@gmail.com>
2016-07-13 10:44:03 +0900

    implement an algorithm of markov chain

--- common.py
+++ common.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 
 import dotenv
+import random
+import yaml
 from os.path import join, dirname, abspath
 
 
@@ -9,4 +11,25 @@ def denv(envkey):
 
 
 def markov():
-    return 'aaa'
+    f = open(join(abspath(dirname(__file__)), 'table.yml'), 'r')
+    table = yaml.load(f)
+    f.close()
+    if table:
+        period = [u'。', u'！', u'？']
+        punc = [u'・', u'、', u'「', u'」']
+        current_wd = random.choice(table.keys())
+        while current_wd in period or current_wd in punc:
+            current_wd = random.choice(table.keys())
+
+        result = [current_wd]
+        while current_wd not in period:
+            next_wd = random.choice(table[current_wd])
+            if not next_wd:
+                break
+            result.append(next_wd)
+            current_wd = next_wd
+
+        sentence = ''.join(result)
+        return sentence
+    else:
+        return None
